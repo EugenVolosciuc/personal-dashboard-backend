f58b4ca71e1ddcfe376162f50621ff0741e5518a
Eugen Volosciuc <volosciuc.eugen@gmail.com>
2021-02-07 20:59:16 +0200

    Work on session persistance with redis

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
+const { v4: uuid } = require('uuid');
+// const redis = require('redis');
+const redisStore = require('connect-redis')(session);
 
 const connectToDB = require('./database/connect');
 const { handleError } = require('./utils/errorHandler');
 const { passport, initializePassport } = require('./config/passport');
 
+// const redisClient = redis.createClient();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+console.log("process.env.NODE_ENV")
+
+// Log redis error
+// redisClient.on('error', (err) => {
+//   console.log('Redis error: ', err);
+// });
 
 // CONFIG
 require('dotenv').config();
 app.use(express.json());
-app.use(cors({ 
-    credentials: true, 
-    origin: [process.env.FRONTEND_LOCAL_LINK, process.env.FRONTEND_DEV_LINK] // TODO: add production link when will launch product
-})); 
+app.use(cors({
+  credentials: true,
+  origin: [process.env.FRONTEND_LOCAL_LINK, process.env.FRONTEND_DEV_LINK] // TODO: add production link when will launch product
+}));
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: false
+  genid: () => {
+    return uuid()
+  },
+  ...(process.env.NODE_ENV === 'production'
+    ? { store: new redisStore({ host: process.env.REDIS_DEV_HOST, port: process.env.REDIS_DEV_PORT, client: redisClient }) }
+    : {}
+  ),
+  name: 'REDIS_SESSION_CACHE',
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  name: "PD_SESSION",
+  cookie: { secure: true, maxAge: 60000 * 60 * 24 }, // 1 minute * 60 minutes * 24 hours = 1 day
+  saveUninitialized: false
 }));
 
 connectToDB();
@@ -43,7 +65,7 @@ app.use('/widget-positions', require('./routes/widgetPositionRoutes'));
 
 // ERROR HANDLING
 app.use((err, req, res, next) => {
-    handleError(err, res);
+  handleError(err, res);
 });
 
 // RUN SERVER
